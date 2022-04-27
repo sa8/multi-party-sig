599fdfcd3762ce51bef26b0b31fab07886d25619
Sarah Azouvi <sarah8@bitbucket.org>
2022-04-27 13:05:59 +0100

    print messages

--- pkg/protocol/handler.go
+++ pkg/protocol/handler.go
@@ -13,7 +13,7 @@ import (
 	"github.com/sa8/multi-party-sig/pkg/party"
 )
 
-var timeOut = (time.Second) * 5
+var timeOut = (time.Second) * 3
 
 // StartFunc is function that creates the first round of a protocol.
 // It returns the first round initialized with the session information.
@@ -98,6 +98,8 @@ func (h *MultiHandler) Listen() <-chan *Message {
 // CanAccept returns true if the message is designated for this protocol protocol execution.
 func (h *MultiHandler) CanAccept(msg *Message) bool {
 	r := h.currentRound
+	fmt.Println("Can accept round:", r.Number())
+	fmt.Println("message round number: ", msg.RoundNumber)
 	if msg == nil {
 		return false
 	}
