59de9ad9fdfc820b57ef577d99cdd7154a6bf5f6
Sarah Azouvi <sarah8@bitbucket.org>
2022-04-27 19:23:12 +0100

    increased timeout again to try and scale

--- pkg/protocol/handler.go
+++ pkg/protocol/handler.go
@@ -13,7 +13,7 @@ import (
 	"github.com/sa8/multi-party-sig/pkg/party"
 )
 
-var timeOut = (time.Second) * 5
+var timeOut = (time.Second) * 8
 
 // StartFunc is function that creates the first round of a protocol.
 // It returns the first round initialized with the session information.
